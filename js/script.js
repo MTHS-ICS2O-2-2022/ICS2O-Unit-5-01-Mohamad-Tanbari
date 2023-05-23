2d7d2b16a33a9646ce951453da3adea76f4809d7
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-05-23 21:12:54 +0000

    ready for testing

--- js/script.js
+++ js/script.js
@@ -8,13 +8,14 @@
 
 // This code generates a random number between 1 and 5 that is required for the guessNumber() function
 const randomNumber = Math.floor(Math.random() * 5) + 1
+console.log(randomNumber)
 
 /*
 * This function checks if the number guessed is correct
 */
 function guessNumber() {
   // Get user input
-  const guess = parseint(document.getElementById('guess').value)
+  const guess = parseInt(document.getElementById('guess').value)
 
   // Check if guess is correct
   if (guess == randomNumber) {
